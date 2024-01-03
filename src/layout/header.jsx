d227dc9d33f3970e092d5a9027909848e1580989
wendychaung <7518647+wendychaung@users.noreply.github.com>
2024-01-03 14:48:55 +0800

    update fonts

--- src/layout/header.jsx
+++ src/layout/header.jsx
@@ -1,8 +1,8 @@
 import { Button } from 'antd';
-// import Logo from "../../assets/images/logo.svg";
+import Logo from "../assets/images/logo.png";
 import {SearchOutlined, MenuOutlined} from "@ant-design/icons";
-// import InscribeImg from "../../assets/images/inscribe.svg";
-// import MarketImg from "../../assets/images/market.svg";
+import InscribeImg from "../assets/images/inscribe.svg";
+import MarketImg from "../assets/images/market.svg";
 
 import { NavLink } from "react-router-dom";
 import styled from "styled-components";
@@ -14,12 +14,46 @@ const Box = styled.div`
     }
 `
 
+const LogoBox = styled.div`
+  display: flex;
+  align-items: center;
+  margin-top: 40px;
+  a{
+    text-decoration: none;
+    font-family: Aeonik Mono;
+  }
+  span{
+    font-size: 48px;
+    color: #fff;
+    line-height: 60px;
+    text-decoration: none;
+  }
+  img{
+    height:90px;
+    //background: #000;
+    margin-bottom: 30px;
+  }
+`
+
 export default function Header() {
     return <div className="app-header">
-        <div className="logo">
-            {/*<NavLink to="/"><img src={Logo} alt=""/></NavLink>*/}
-            seeU
-        </div>
+        <LogoBox>
+            <NavLink to="/"><img src={Logo} alt=""/></NavLink>
+        </LogoBox>
+        <Box className="menu">
+            <NavLink  to="/explorer" className={({ isActive }) => isActive ? 'menu-item active' : 'menu-item false'}>
+                <SearchOutlined />
+                <span className="label">Explorer</span>
+            </NavLink>
+            <NavLink  to="/marketplace" className={({ isActive }) => isActive ? 'menu-item active' : 'menu-item false'}>
+                <img src={MarketImg} alt=""/>
+                <span className="label">Marketplace</span>
+            </NavLink>
+            <NavLink to="/inscribe"  className={({ isActive }) => isActive ? 'menu-item active' : 'menu-item false'}>
+                <img src={InscribeImg} alt=""/>
+                <span className="label">Inscribe</span>
+            </NavLink>
+        </Box>
         <div className="connect">
             <Button  type="primary" className="connect-button">Connect</Button>
         </div>
