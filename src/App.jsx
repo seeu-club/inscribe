abd723b2d134d024609213ab384e090cbac6cf65
wendychaung <7518647+wendychaung@users.noreply.github.com>
2024-01-23 00:51:18 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import {BrowserRouter as Router} from "react-router-dom";
+import {HashRouter as Router} from "react-router-dom";
 import RouterLink from "./router/router";
 import GlobalStyle from "./utils/GlobalStyle";
 import store,{persistor} from "./store";
